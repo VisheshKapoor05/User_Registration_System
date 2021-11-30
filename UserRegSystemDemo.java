d388955a9edd0b0f1217130ffaca70f6829ef531
VisheshKapoor05 <vishesh17kapoor@gmail.com>
2021-11-30 14:09:27 +0530

    UC2: Added method validating last name

--- UserRegSystemDemo.java
+++ UserRegSystemDemo.java
@@ -6,11 +6,18 @@ public class UserRegSystemDemo {
 	public static void main(String[] args) {
 		Scanner sc = new Scanner(System.in);
 		
-		System.out.println("Enter firstName: ");
+		System.out.println("Welcome to the User Registration System");
+		System.out.println();
+		
+		System.out.println("Enter first Name: ");
 		String firstName = sc.next();
+		
+		System.out.println("Enter last Name: ");
+		String lastName = sc.next();
 		
 		UserValidationService validationService = new UserValidationService();
 		validationService.validFirstName(firstName);
+		validationService.validLastName(lastName);
 	}
 
 }
--- UserValidationService.java
+++ UserValidationService.java
@@ -6,18 +6,26 @@ import java.util.regex.Pattern;
 public class UserValidationService {
 	
 	private String FIRST_NAME_REGEX = "^[A-Z]{1}[a-z]{2,}$";
+	private String LAST_NAME_REGEX  = "^[A-Z]{1}[a-z]{2,}$";
 	
 	
 	public void validFirstName(String firstName) {
-		
 		Pattern pattern = Pattern.compile(FIRST_NAME_REGEX);
 		Matcher matcher = pattern.matcher(firstName);
-		
 		if(matcher.matches())
-			System.out.println("You entered a valid first name");
+			System.out.println("You've entered a valid first name");
 		else
 			System.out.println("Please enter a valid first name");
-		
+	}
+	
+	
+	public void validLastName(String lastName) {
+		Pattern pattern = Pattern.compile(LAST_NAME_REGEX);
+		Matcher matcher = pattern.matcher(lastName);
+		if(matcher.matches())
+			System.out.println("You've entered a valid last name");
+		else
+			System.out.println("Please enter a valid last name");
 	}
 
 }
